9cbc2ca636334215bbcb3971373151c42b02c8c0
Dmitry Zuikov <dzuikov@gmail.com>
2023-01-02 09:29:30 +0300

    module to file guess

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,25 @@
+{-# Language OverloadedStrings #-}
 module Main where
 
-import Data.Functor
 import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Maybe
 import Data.Attoparsec.Text
 import Data.Char qualified as Char
 import Data.Data
+import Data.Functor
 import Data.Generics.Uniplate.Data()
 import Data.Generics.Uniplate.Operations
+import Data.Maybe
 import Data.Text.IO qualified as Text
 import Data.Text qualified as Text
 import Data.Text (Text)
 import Options.Applicative as O
+import Prettyprinter
 import Safe
 import System.Directory
 import System.FilePath.Posix
-import Prettyprinter
+import System.FilePattern.Directory
 
 data Opts = OptHs Bool
             deriving (Eq,Ord,Show,Data)
@@ -31,6 +36,7 @@ main = join . customExecParser (prefs showHelpOnError) $
     parser ::  O.Parser (IO ())
     parser = hsubparser (  command "touch"  (info pTouch  (progDesc "touch dir/file recursively"))
                         <> command "module" (info pModule (progDesc "get module name from path or file"))
+                        <> command "path"   (info pPath    (progDesc "guess module path from name"))
                         )
 
     pTouch = do
@@ -42,6 +48,10 @@ main = join . customExecParser (prefs showHelpOnError) $
     pModule = do
       n <- optional $ strArgument ( metavar "PATH" )
       pure $ runModule n
+
+    pPath= do
+      n <- optional $ strArgument ( metavar "NAME" )
+      pure $ runPath n
 
 
 itsModule :: Text -> Bool
@@ -62,6 +72,31 @@ runModule :: Maybe FilePath -> IO ()
 runModule (Just fp) = putStrLn (makeHaskellModule fp)
 
 runModule Nothing = getContents >>= putStrLn . makeHaskellModule
+
+
+runPath :: Maybe FilePath -> IO ()
+runPath fp = maybe fromStdin pure fp >>= makeGuess
+  where
+    fromStdin = getContents
+
+    makeGuess :: FilePath -> IO ()
+    makeGuess n = void $ runMaybeT $ do
+      name <- MaybeT $ pure
+                     $ headMay
+                     $ filter validModule
+                     $ foldMap Text.words
+                     $ Text.lines (Text.pack n)
+
+      -- liftIO $ print name
+
+      let fn = makeFileName (Text.unpack name)
+      let path = "**/" <> fn
+
+      variants <- MaybeT $ liftIO
+                         $ getDirectoryFilesIgnore "." [path] [".*/**", "dist-newstyle/**"] <&> listToMaybe
+
+      liftIO $ putStrLn variants
+
 
 runTouch :: Bool -> Bool -> FilePath -> IO ()
 
@@ -96,6 +131,11 @@ runTouch False False s = do
     createDirectoryIfMissing True dir
     Text.writeFile  path ""
     makeRelativeToCurrentDirectory path >>= putStrLn
+
+makeFileName :: String -> FilePath
+makeFileName f = addExtension (Text.unpack $ Text.replace "." "/" tf) ".hs"
+  where
+    tf = Text.pack f
 
 makeHaskellModule :: FilePath -> String
 makeHaskellModule s = s0
